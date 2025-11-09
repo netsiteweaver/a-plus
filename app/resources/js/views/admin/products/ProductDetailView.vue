b33816823afaf77cb22571636aa09b28bc31eca6
SimpleGuy <reeazr@gmail.com>
2025-11-09 16:41:41 +0400

    Fix 422 error: Change include_values from boolean to integer

--- app/resources/js/views/admin/products/ProductDetailView.vue
+++ app/resources/js/views/admin/products/ProductDetailView.vue
@@ -147,7 +147,7 @@ async function loadCategories() {
 
 async function loadAttributes() {
     try {
-        const response = await catalogApi.listAttributes({ per_page: 200, include_values: true });
+        const response = await catalogApi.listAttributes({ per_page: 200, include_values: 1 });
         attributes.value = response.data?.data ?? [];
     } catch (err) {
         console.error('Error loading attributes:', err);
